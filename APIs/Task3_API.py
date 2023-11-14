c763938594454269b3c0e78b0e08ed4eea1d6128
Photon_31 <92568088+Minz31@users.noreply.github.com>
2023-11-14 22:07:27 +0530

    APIs_cmt

--- APIs/Task3_API.py
+++ APIs/Task3_API.py
@@ -7,22 +7,29 @@ app = FastAPI()
 def index():
     return {'WELCOME TO ICC WORLDCUP PREDICTION!!'}
 
-@app.get("/Top 6 Bowlers of INDIA!!")
+@app.get("/Semi-Quailfier")
 def read_root():
     # Load the stored output from the pickle file
-    with open('output3.pkl', 'rb') as output_file:
+    with open('output1.pkl', 'rb') as output_file:
         stored_output = pickle.load(output_file)
 
     return stored_output
 
-@app.get("/Prediction of 11 Players for INDIA!!")
+@app.get("/Finalists")
 def read_root():
     # Load the stored output from the pickle file
-    with open('output4.pkl', 'rb') as output_file:
+    with open('output2.pkl', 'rb') as output_file:
         stored_output = pickle.load(output_file)
 
     return stored_output
 
+@app.get("/Final_Winner")
+def read_root():
+    # Load the stored output from the pickle file
+    with open('output.pkl', 'rb') as output_file:
+        stored_output = pickle.load(output_file)
+
+    return stored_output
 
 # Run the API with uvicorn
 if __name__ == '__main__':
